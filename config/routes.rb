7032974df648193db82cf727fa524ea2f1bdaf42
Aaron Jacobson & Angelica Gonzalez & Mobolaji David <pair+SudoSanauu.agonzalez0515.mobolaji89@devbootcamp.com>
2016-12-07 20:28:10 -0800

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'root#index'
+
+  resource :school, only: [:show] do
+    resources :teachers
+    resources :students do
+      patch  "/teams", to: 'student#update_team'
+      delete "/teams", to: 'student#destroy_team'
+    end
+  end
 end
