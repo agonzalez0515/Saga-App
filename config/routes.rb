4b46bf780bc78cff865c25d790ca02e5e435a791
Aaron Jacobson & Angelica Gonzalez & Mobolaji David <pair+SudoSanauu.agonzalez0515.mobolaji89@devbootcamp.com>
2016-12-08 10:18:21 -0800

    fix school routes and added session routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'root#index'
 
-  resource :school, only: [:show] do
+  resources :schools, only: [:show] do
     resources :teachers
     resources :students do
       patch  "/teams", to: 'student#update_team'
       delete "/teams", to: 'student#destroy_team'
     end
   end
+
+  get "/login", to: 'sessions#new'
+  post "/login", to: 'sessions#create'
+  delete "/logout", to: 'sessions#destroy'
 end
