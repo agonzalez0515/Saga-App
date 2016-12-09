65c62a38b4031da942d726a9cec71d691ebb1425
Aaron Jacobson & Mobolaji David <pair+SudoSanauu.mobolaji89@devbootcamp.com>
2016-12-09 11:53:09 -0800

    make current_teach a helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :current_teacher # this is required for header
+  helper_method :current_teacher
 
   private
   def current_teacher
