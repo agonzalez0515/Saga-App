4f994418a20b285dfd2c40f9fa412eb65df22844
Angelica Gonzalez & Megan Eding <pair+agonzalez0515.meganeding1@devbootcamp.com>
2016-12-09 14:38:57 -0800

    Seeds: update teacher/school/team assoc

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,5 @@
 class TeachersController < ApplicationController
-  before_action :require_login
+  before_action :require_login, except: ['new', 'create']
   before_action :check_school_match
 
   def index
