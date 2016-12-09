3e25a5afcaceb737004b36edc2fb8d30f43619ea
Aaron Jacobson & Mobolaji David <pair+SudoSanauu.mobolaji89@devbootcamp.com>
2016-12-08 17:43:57 -0800

    create nav bar and flash partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_teacher
 
   private
   def current_teacher
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
   def login(teacher)
     session[:teacher_id] = teacher.id
   end
+
+  # def logged_in?
+  #   !!session[:teacher_id]
+  # end
 
   def is_admin?
     current_teacher.admin
